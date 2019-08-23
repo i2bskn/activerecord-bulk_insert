ac453c989c2976f4c52ce11a049d8a1eca93eded
i2bskn <i2bskn@craftake.com>
2019-08-23 22:57:10 +0900

    Fix spec

--- spec/active_record/bulk_insert_spec.rb
+++ spec/active_record/bulk_insert_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Activerecord::BulkInsert do
   it "has a version number" do
     expect(Activerecord::BulkInsert::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
