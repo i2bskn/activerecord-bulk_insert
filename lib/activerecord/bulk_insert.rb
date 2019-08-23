589a82e458610c403e479422a616034c8baa6655
i2bskn <i2bskn@craftake.com>
2019-08-23 19:43:46 +0900

    initial commit

---
+++ lib/activerecord/bulk_insert.rb
@@ -0,0 +1,8 @@
+require "activerecord/bulk_insert/version"
+
+module Activerecord
+  module BulkInsert
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/bulk_insert/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module BulkInsert
+    VERSION = "0.1.0"
+  end
+end
